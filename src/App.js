3f8aabe830caef673cad732fa03968fd011c2abf
yabby <yabby@Seunghun-MacBookPro.local>
2021-01-01 00:26:19 +0900

    #2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import Potato from "./Potato";
+
 function App() {
   return (
     <div className="App">
-      Hello!!!!!
+      <h1>Hello!!!!!</h1>
+      <Potato></Potato>
     </div>
   );
 }
